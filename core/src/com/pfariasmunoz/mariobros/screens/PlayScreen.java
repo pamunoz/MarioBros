2e89da96d97ec66b35008f357f6e3c46de5c7a3f
Pablo Munoz <pfariasmunoz@gmail.com>
2016-07-31 16:03:10 -0400

    add tmx map loader

--- core/src/com/pfariasmunoz/mariobros/screens/PlayScreen.java
+++ core/src/com/pfariasmunoz/mariobros/screens/PlayScreen.java
@@ -4,28 +4,67 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.pfariasmunoz.mariobros.MarioBros;
+import com.pfariasmunoz.mariobros.scenes.Hud;
 
 /**
  * Created by Pablo Farias on 31-07-16.
  */
 public class PlayScreen implements Screen {
     private MarioBros game;
-    Texture texture;
     private OrthographicCamera gamecam;
     private Viewport gamePort;
+    private Hud hud;
+
+    //Tiled map variables
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+    // Box2d variables
+    private World world;
+    private Box2DDebugRenderer b2dr;
 
 
     public PlayScreen(MarioBros game) {
         this.game = game;
-        texture = new Texture("badlogic.jpg");
+        // create cam used to follow mario through cam world
         gamecam = new OrthographicCamera();
-        gamePort = new FitViewport(800, 480, gamecam);
+        // create a FitViewport to maintain virtual aspect ration despite of device
+        gamePort = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, gamecam);
+        // create aur game HUD for scores/timers/level info
+        hud = new Hud(game.batch);
+
+        // Load our map and setup our map renderer
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+
+        // initially set our gamecam to be centered correctly at the start of the map
+        gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
+        world = new World(new Vector2(0, 0), true);
+        b2dr = new Box2DDebugRenderer();
+    }
+
+    public void handleInput(float dt) {
+        if(Gdx.input.isTouched()) gamecam.position.x += 100 * dt;
+    }
+
+    public void update(float dt) {
+        handleInput(dt);
+        gamecam.update();
+
+        renderer.setView(gamecam);
     }
 
     @Override
@@ -33,14 +72,24 @@ public class PlayScreen implements Screen {
 
     }
 
+
+
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+
+        update(delta);
+        //clear the game screen with Black
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.setProjectionMatrix(gamecam.combined);
-        game.batch.begin();
-        game.batch.draw(texture, 0, 0);
-        game.batch.end();
+
+        renderer.render();
+        // Set our batch to now draw what the Hud camera sees.
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+
+
+
+
     }
 
     @Override
